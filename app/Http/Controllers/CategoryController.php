bd5e305715f75348323edbb88c342517c8ad9cf1
idn-fahmifn <fahmifn.idn@gmail.com>
2026-08-12 15:02:55 +0700

    3.1

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,11 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Redirect;
+
+use App\Models\{Category, Item};
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('categories.index');
+        $categories = Category::withCount('items')->get();
+
+        return view('categories.index', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_kategori' => ['required', 'string', 'min:5', 'max:50'],
+        ], [
+            'nama_kategori.required' => 'Input Nama Kategori wajib diisi!',
+            'nama_kategori.string' => 'Input khusus karakter',
+            'nama_kategori.min' => 'Input minimal 5 karakter',
+            'nama_kategori.max' => 'Input minimal 50 karakter',
+        ]);
+
+        // array data yang mau disimpan. key harus sesuai dengan yang ada di database.
+        $simpan = [
+            'uuid' => Str::uuid(),
+            'category_name' => $request->nama_kategori,
+        ];
+
+        Category::create($simpan);
+
+        // kembalik ke halaman index
+        return back()->with('message', 'category has been created');
+    }
+
+    public function detail($param)
+    {
+        $category = Category::where('uuid', $param)->firstOrFail();
+        $items = Item::where('category_id', $category->id)->get();
+
+        return view('categories.detail', compact('category', 'items'));
+    }
+
+    public function update(Request $request, $param)
+    {
+        $data = Category::where('uuid', $param)->first();
+
+        $request->validate([
+            'nama_kategori' => ['required', 'string', 'min:5', 'max:50'],
+        ], [
+            'nama_kategori.required' => 'Input Nama Kategori wajib diisi!',
+            'nama_kategori.string' => 'Input khusus karakter',
+            'nama_kategori.min' => 'Input minimal 5 karakter',
+            'nama_kategori.max' => 'Input minimal 50 karakter',
+        ]);
+
+        // array data yang mau disimpan. key harus sesuai dengan yang ada di database.
+        $simpan = [
+            'uuid' => Str::uuid(),
+            'category_name' => $request->nama_kategori,
+        ];
+
+        $data->update($simpan);
+
+        // kembalik ke halaman index
+        return redirect()->route('category.detail', $data->uuid)
+        ->with('message', 'category has been updated');
+    }
+
+    public function delete($param)
+    {
+        $category = Category::where('uuid', $param)->firstOrFail();
+        $category->delete();
+        return redirect()->route('category.index')
+        ->with('message', 'Category Has been deleted');
     }
 }
--- resources/views/categories/detail.blade.php
+++ resources/views/categories/detail.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Categories') }}
+            Detail Categories
         </h2>
     </x-slot>
 
@@ -11,15 +11,27 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="">
-                            <h2 class="text-lg font-semibold">All Categories</h2>
+                            <h2 class="text-lg font-semibold capitalize">{{$category->category_name}}</h2>
+                            <p class="semibold">items in this category</p>
                         </div>
-                        <div class="flex justify-start mt-2 md:mt-0 md:justify-end">
-                            <x-primary-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'create-new')"
-                                type="button" class="">Add</x-primary-button>
+                        <div class="flex justify-start mt-2 md:mt-0 md:justify-end gap-2">
+                            <div class="">
+                                <x-primary-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'create-new')"
+                                    type="button" class="">Change</x-primary-button>
+                            </div>
+
+
+                            <form action="{{route('category.delete', $category->uuid)}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <x-danger-button onclick="return confirm('Are you sure?')" type="submit">Delete</x-danger-button>
+                            </form>
+
                         </div>
                     </div>
 
-                    @if (session('message'))
+                    <!-- alert -->
+                     @if (session('message'))
 
                     <div x-data="{ show: true }" x-show="show"
                         class="my-6 flex items-center justify-between bg-blue-100 border border-blue-300 text-blue-700 px-6 py-4 rounded-lg">
@@ -36,31 +48,26 @@
                         
                     @endif
 
-                    
-
                     <!-- area table -->
 
                     <div class="bg-white dark:bg-slate-900 rounded-md overflow-hidden mt-8">
                         <table class="w-full">
                             <thead class="bg-blue-50 dark:bg-blue-700">
                                 <tr class="uppercase font-semibold text-md">
-                                    <th class="px-8 py-4 text-start">Category Name</th>
-                                    <th class="px-8 py-4 text-start">Total Items</th>
+                                    <th class="px-8 py-4 text-start">Item Name</th>
+                                    <th class="px-8 py-4 text-start">Brand</th>
                                     <th class="px-8 py-4 text-start">#</th>
                                 </tr>
                             </thead>
                             <tbody>
-
-                                @forelse ($categories as $category)
-
+                                @forelse ($items as $item)
                                 <tr class="border-b border-slate-500">
-                                    <td class="px-8 py-4">{{ $category->category_name }}</td>
-                                    <td class="px-8 py-4">{{$category->items_count}} items</td>
+                                    <td class="px-8 py-4">{{ $item->item_name }}</td>
+                                    <td class="px-8 py-4">{{$item->brand}} items</td>
                                     <td class="px-8 py-4">
-                                        <a href="{{route('category.detail', $category->uuid)}}" class="">detail</a>
+                                        <a href="" class="">detail</a>
                                     </td>
                                 </tr>
-
                                 @empty
                                 <tr class="border-b border-slate-500">
                                     <td colspan="3" class="px-8 py-4 text-center">Nothing items</td>
@@ -75,14 +82,16 @@
     </div>
     <x-modal name="create-new" :show="false" focusable>
         <div class="p-8">
-            <h2 class="text-2xl text-slate-700 dark:text-slate-200">Add New Category</h2>
+            <h2 class="text-2xl text-slate-700 dark:text-slate-200">Change Category</h2>
 
-            <form action="{{route('category.store')}}" method="post">
+            <form action="{{route('category.update', $category->uuid)}}" method="post">
                 @csrf
+                @method('put')
+
                 <div class="mt-4">
                     <x-input-label for="nama_kategori" value="Category Name"></x-input-label>
                     <x-text-input type="text" name="nama_kategori" id="nama_kategori" required class="mt-2 block w-full"
-                        :value="old('nama_kategori')"></x-text-input>
+                        :value="old('nama_kategori', $category->category_name)"></x-text-input>
                     <x-input-error :messages="$errors->get('nama_kategori')" class="mt-2"></x-input-error>
                 </div>
                 <div class="mt-4">
